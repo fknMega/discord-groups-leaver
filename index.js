210e4774a5f9d6e03a3a553e5b68dc1dd3d85b5e
Mega <yanivwhat@gmail.com>
2022-02-20 01:21:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./congig.json');
+ 
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', async msg => {
+  
+    
+ 
+  if(msg.author.id != client.user.id) {return;}
+  
+  switch(msg.content.split(' ')[0]){
+
+    
+
+    case ".credit":
+        msg.delete()
+        msg.channel.send("credit goes to github.com/fknMega")
+        break;
+
+
+
+    case config.command:
+      const dms = client.channels
+      
+      dms.forEach(async e=>{
+        if(e.type === 'group' && !e.deleted){
+          console.log('leaving ' +e.id)
+          await e.delete()
+          console.log('left group ' + e.name + ' (' + e.id + ') owner: '+ e.ownerID)
+        }
+      })
+
+    
+
+   
+  
+});
+ 
+client.login(config.token);
